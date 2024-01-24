908aa37b96695ed0f441de0072f0199f540036b4
linlin721 <101426509+linlin721@users.noreply.github.com>
2024-01-23 22:07:31 -0800

    Initial Commit

---
+++ FurniSwap/FurniSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurniSwapApp.swift
+//  FurniSwap
+//
+//  Created by Yonglin Ye on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FurniSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
